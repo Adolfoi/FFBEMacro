71fb405ca99c3e4a2e6fb5d0ca555f3c4bef5061
Mitsuhiro Hashimoto <adolfoi.ios.developer@gmail.com>
2016-09-15 20:28:20 +0900

    FFBEMacro.lua
    lua script.

---
+++ FFBEMacro.lua
@@ -0,0 +1,115 @@
+local color = getColor(164,14);
+
+if color == 0 then
+
+touchDown(0,310,720);
+usleep(10000);
+touchUp(0,310,720);
+
+end
+
+usleep(2000000);
+
+color = getColor(306,1002)
+while color ~= 16777215 do
+color = getColor(266,704)
+if color == 10337 then
+touchDown(0,266,704);
+usleep(10000);
+touchUp(0,266,704);
+usleep(2000000);
+end
+
+color = getColor(164,14);
+if color == 0 then
+touchDown(0,310,720);
+usleep(10000);
+touchUp(0,310,720);
+usleep(2000000);
+end
+
+
+usleep(2000000);
+color = getColor(306,1002)
+end
+
+
+
+	
+
+
+
+color = getColor(306,1002)
+
+if color == 16777215 then
+touchDown(0,306,1002);
+usleep(10000);
+touchUp(0,306,1002);
+	
+end
+
+usleep(2000000);
+
+touchDown(0,320,400);
+usleep(10000);
+touchUp(0,320,400);
+
+usleep(2000000);
+
+color = getColor(284,964)
+
+if color == 65502 then
+touchDown(0,292,998);
+usleep(10000);
+touchUp(0,292,998);
+	
+end
+
+usleep(7000000);
+
+touchDown(0,80,1094);
+usleep(10000);
+touchUp(0,80,1094);
+
+
+
+usleep(40000000);
+
+
+touchDown(0,320,1000);
+usleep(10000);
+touchUp(0,320,1000);
+
+usleep(1000000);
+
+touchDown(0,320,1000);
+usleep(10000);
+touchUp(0,320,1000);
+
+usleep(1000000);
+
+touchDown(0,320,1000);
+usleep(10000);
+touchUp(0,320,1000);
+
+usleep(1000000);
+
+touchDown(0,320,1000);
+usleep(10000);
+touchUp(0,320,1000);
+
+usleep(1000000);
+
+
+touchDown(0,320,1000);
+usleep(10000);
+touchUp(0,320,1000);
+
+usleep(1000000);
+touchDown(0,320,1000);
+usleep(10000);
+touchUp(0,320,1000);
+
+usleep(1000000);
+
+
